765f516ea3bfd9553d2510c5058f4d82ea68e06d
HansMuguerza <hansmuguerza14@gmail.com>
2021-09-17 17:03:34 -0500

    cambio1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React APP
+          React APP 2.0v
         </p>
         <a
           className="App-link"
